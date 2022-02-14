0c05b6067adeab06d1b8aaccb916b769bbfd97cc
metaclep <cleepe5@hotmail.com>
2022-02-14 06:45:47 -0300

    Rotas de páginas e Container criados.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import {} from "react";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
+import { Link, Routes, Route } from "react-router-dom";
+import Container from "./components/layout/Container";
 
 function App() {
   return (
     <div>
-      <h1>Cost</h1>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/Company">Empresa</Link>
+        <Link to="/Contact">Contato</Link>
+        <Link to="/NewProject">Novo Projeto</Link>
+      </ul>
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Company" element={<Company />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/NewProject" element={<NewProject />} />
+        </Routes>
+      </Container>
+      <footer>Footer</footer>
     </div>
   );
 }
---
+++ src/components/pages/NewProject.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NewProject() {
+  return <h1>NewProject</h1>;
+}
