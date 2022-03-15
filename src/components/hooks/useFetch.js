dc3248b1a421fa86a2416f6fbf7182f570584b69
metaclep <cleepe5@hotmail.com>
2022-03-15 09:28:57 -0300

    Limpeza

--- src/components/hooks/useFetch.js
+++ src/components/hooks/useFetch.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 const api = axios.create({
   baseURL: "http://localhost:3004",
